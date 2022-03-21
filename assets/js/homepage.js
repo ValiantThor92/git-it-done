368aa2d7fd7becd06e9361ee6d764296f73d027c
unknown <travislnelson92@gmail.com>
2022-03-21 14:04:17 -0700

    debugg

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,15 @@
-var getUserRepos = function() {
+var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-    // make a request to the apiUrl
+  
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
+      console.log(response);
+      response.json().then(function(data) {
+        console.log(data);
+      });
     });
-};
-
-getUserRepos();
+  };
+  
+  getUserRepos("valiantthor92");
+  
